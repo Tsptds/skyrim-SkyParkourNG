63ffdabf566f2fa84a2a664762ecb93176344c7c
Tsptds <152429392+Tsptds@users.noreply.github.com>
2025-07-24 18:28:19 +0300

    Update CameraHandler.hpp

--- include/Hooks/CameraHandler.hpp
+++ include/Hooks/CameraHandler.hpp
@@ -7,82 +7,125 @@ namespace Hooks {
             static bool InstallCamStateHooks();
 
         private:
-            static bool Install_CanProcess_TPP();
-            static bool Install_Update_TPP();
-            static bool Install_CanProcess_FPP();
+            struct TPP {
+                    struct Install {
+                            static bool CanProcess();
+                            static bool Update();
+                            static bool Begin();
+                    };
 
-            // Our hook callbacks
-            static bool CanProcess_TPP(RE::ThirdPersonState *a_this, RE::InputEvent *a_event);
-            static void Update_TPP(RE::ThirdPersonState *a_this, RE::BSTSmartPointer<RE::TESCameraState> &a_nextState);
+                    struct Callback {
+                            static bool CanProcess(RE::ThirdPersonState *a_this, RE::InputEvent *a_event);
+                            static void Update(RE::ThirdPersonState *a_this, RE::BSTSmartPointer<RE::TESCameraState> &a_nextState);
+                    };
 
-            static bool CanProcess_FPP(RE::FirstPersonState *a_this, RE::InputEvent *a_event);
+                    struct OG {
+                            static inline REL::Relocation<decltype(Callback::CanProcess)> _CanProcess;
+                            static inline REL::Relocation<decltype(Callback::Update)> _Update;
+                    };
+            };
 
-            // Originals
-            static inline REL::Relocation<decltype(CanProcess_TPP)> _CanProcess_TPP;
-            static inline REL::Relocation<decltype(Update_TPP)> _Update_TPP;
+            struct FPP {
+                    struct Install {
+                            static bool CanProcess();
+                            static bool Update();
+                            static bool Begin();
+                    };
 
-            static inline REL::Relocation<decltype(CanProcess_FPP)> _CanProcess_FPP;
+                    struct Callback {
+                            static bool CanProcess(RE::FirstPersonState *a_this, RE::InputEvent *a_event);
+                            static void Update(RE::FirstPersonState *a_this, RE::BSTSmartPointer<RE::TESCameraState> &a_nextState);
+                    };
+
+                    struct OG {
+                            static inline REL::Relocation<decltype(Callback::CanProcess)> _CanProcess;
+                            static inline REL::Relocation<decltype(Callback::Update)> _Update;
+                    };
+            };
     };
 }  // namespace Hooks
 
-/* Install */
+// Install
+
 bool Hooks::CameraHandler::InstallCamStateHooks() {
-    bool res = Install_CanProcess_TPP();
-    res &= Install_Update_TPP();
-    res &= Install_CanProcess_FPP();
+    bool res = false;
+
+    res &= TPP::Install::CanProcess();
+    res &= TPP::Install::Update();
+
+    res &= FPP::Install::CanProcess();
+    res &= FPP::Install::Update();
 
     return res;
 }
 
-bool Hooks::CameraHandler::Install_CanProcess_TPP() {
+/* Third person */
+bool Hooks::CameraHandler::TPP::Install::CanProcess() {
     /* VTABLE 0 ->TesCameraState /  1 ->PlayerInputHandler */
 
     REL::Relocation<uintptr_t> vtblInput{RE::VTABLE_ThirdPersonState[1]};
-    _CanProcess_TPP = vtblInput.write_vfunc(0x1, &CanProcess_TPP);
+    OG::_CanProcess = vtblInput.write_vfunc(0x1, &Callback::CanProcess);
 
-    if (!_CanProcess_TPP.address()) {
+    if (!OG::_CanProcess.address()) {
         logger::critical("TPP CanProcess Hook Not Installed");
         return false;
     }
     return true;
 }
-bool Hooks::CameraHandler::Install_Update_TPP() {
+bool Hooks::CameraHandler::TPP::Install::Update() {
     /* VTABLE 0 ->TesCameraState /  1 ->PlayerInputHandler */
 
     REL::Relocation<uintptr_t> vtblInput{RE::VTABLE_ThirdPersonState[0]};
-    _Update_TPP = vtblInput.write_vfunc(0x3, &Update_TPP);
+    OG::_Update = vtblInput.write_vfunc(0x3, &Callback::Update);
 
-    if (!_Update_TPP.address()) {
+    if (!OG::_Update.address()) {
         logger::critical("TPP Update Hook Not Installed");
         return false;
     }
     return true;
 }
+/* ------------------------------------------------------------- */
 
-bool Hooks::CameraHandler::Install_CanProcess_FPP() {
+/* First person */
+bool Hooks::CameraHandler::FPP::Install::CanProcess() {
     /* VTABLE 0 ->TesCameraState /  1 ->PlayerInputHandler */
 
     REL::Relocation<uintptr_t> vtblPlayer{RE::VTABLE_FirstPersonState[1]};
-    _CanProcess_FPP = vtblPlayer.write_vfunc(0x1, &CanProcess_FPP);
+    OG::_CanProcess = vtblPlayer.write_vfunc(0x1, &Callback::CanProcess);
 
-    if (!_CanProcess_FPP.address()) {
+    if (!OG::_CanProcess.address()) {
         logger::critical("FPP State Hook Not Installed");
         return false;
     }
     return true;
 }
+bool Hooks::CameraHandler::FPP::Install::Update() {
+    /* VTABLE 0 ->TesCameraState /  1 ->PlayerInputHandler */
 
-/* Hooks */
-bool Hooks::CameraHandler::CanProcess_TPP(RE::ThirdPersonState *a_this, RE::InputEvent *a_event) {
+    REL::Relocation<uintptr_t> vtblInput{RE::VTABLE_FirstPersonState[0]};
+    OG::_Update = vtblInput.write_vfunc(0x3, &Callback::Update);
+
+    if (!OG::_Update.address()) {
+        logger::critical("FPP Update Hook Not Installed");
+        return false;
+    }
+    return true;
+}
+/* ------------------------------------------------------------- */
+
+// Callbacks
+
+/* Third person */
+bool Hooks::CameraHandler::TPP::Callback::CanProcess(RE::ThirdPersonState *a_this, RE::InputEvent *a_event) {
     if (ModSettings::ModEnabled) {
         if (RuntimeVariables::ParkourInProgress) {
             return false;
         }
     }
 
-    return _CanProcess_TPP(a_this, a_event);
+    return OG::_CanProcess(a_this, a_event);
 }
-void Hooks::CameraHandler::Update_TPP(RE::ThirdPersonState *a_this, RE::BSTSmartPointer<RE::TESCameraState> &a_nextState) {
+void Hooks::CameraHandler::TPP::Callback::Update(RE::ThirdPersonState *a_this, RE::BSTSmartPointer<RE::TESCameraState> &a_nextState) {
     if (RuntimeVariables::ParkourInProgress) {
         /* Prevent Havok from pulling the player towards ground */
         auto ctrl = RE::PlayerCharacter::GetSingleton()->GetCharController();
@@ -100,24 +143,33 @@ void Hooks::CameraHandler::Update_TPP(RE::ThirdPersonState *a_this, RE::BSTSmart
         }
     }
 
-    _Update_TPP(a_this, a_nextState);
+    OG::_Update(a_this, a_nextState);
 }
+/* ------------------------------------------------------------- */
 
-bool Hooks::CameraHandler::CanProcess_FPP(RE::FirstPersonState *a_this, RE::InputEvent *a_event) {
+/* First person */
+bool Hooks::CameraHandler::FPP::Callback::CanProcess(RE::FirstPersonState *a_this, RE::InputEvent *a_event) {
     if (ModSettings::ModEnabled) {
         if (RuntimeVariables::ParkourInProgress) {
-            /* Clamp Player looking angle to prevent weird visuals */
-            auto player = RE::PlayerCharacter::GetSingleton();
-            const auto vertAngle = player->data.angle.x;
-            if (vertAngle > 0.9f) {
-                player->data.angle.x = 0.9f;
-            }
-            else if (vertAngle < -0.9f) {
-                player->data.angle.x = -0.9f;
-            }
             return false;
         }
     }
 
-    return _CanProcess_FPP(a_this, a_event);
+    return OG::_CanProcess(a_this, a_event);
 }
+void Hooks::CameraHandler::FPP::Callback::Update(RE::FirstPersonState *a_this, RE::BSTSmartPointer<RE::TESCameraState> &a_nextState) {
+    if (RuntimeVariables::ParkourInProgress) {
+        /* Clamp Player looking angle to prevent weird visuals */
+        auto player = RE::PlayerCharacter::GetSingleton();
+        const auto vertAngle = player->data.angle.x;
+        if (vertAngle > 0.9f) {
+            player->data.angle.x = 0.9f;
+        }
+        else if (vertAngle < -0.9f) {
+            player->data.angle.x = -0.9f;
+        }
+    }
+
+    OG::_Update(a_this, a_nextState);
+}
+/* ------------------------------------------------------------- */
