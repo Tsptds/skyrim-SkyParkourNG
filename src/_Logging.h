b0769ded238cd32ff5b3be05762a7ad731f704c3
Tsptds <152429392+Tsptds@users.noreply.github.com>
2025-08-13 23:57:05 +0300

    Declutter pch.h
    Move globals to separate file

---
+++ src/_Logging.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* Log switches */
+#ifdef _DEBUG
+
+// #define LOG_CLIMB
+// #define LOG_VAULT
+// #define LOG_STEPS
+// #define LOG_LEDGE_UPDATES
+// #define LOG_CROSSHAIR
+// #define LOG_STEPS_VELOCITY
+
+#endif
